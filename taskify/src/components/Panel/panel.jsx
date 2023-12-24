e0cc7de45f8e383a928d8986002cfd767e0c648e
Patrazzo <petarpetrov092@gmail.com>
2023-12-24 15:10:35 +0200

    Testing Darkmode failed

--- taskify/src/components/Panel/panel.jsx
+++ taskify/src/components/Panel/panel.jsx
@@ -5,8 +5,8 @@ import { Button } from "../Columns/Button/button";
 export const Panel = () => {
   return (
     <div>
-      <div class="p-4 sm:ml-64">
-        <div class="p-4 mt-14 flex flex-col items-center">
+      <div class="p-4 sm:ml-64 ">
+        <div class="p-4 mt-14 flex flex-col items-center ">
           <Button></Button>
           <Columns></Columns>
         </div>
